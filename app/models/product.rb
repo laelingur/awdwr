ac4b3aa871f5a3b85c0c3b569cefaed5a64b7975
laelingur <laelingur@gmail.com>
2011-08-13 22:00:07 -0500

    added css and images. added few validations and changed error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ActiveRecord::Base
   validates_presence_of :title, :description, :image_url
-  validates_numericality_of :price
+  validates_numericality_of :price, :message => 'should be a number #fail'
   validate :price_must_be_at_least_one_cent
   validates_uniqueness_of :title
   validates_format_of :image_url,
                       :with => %r{\.(gif|jpg|png)}i,
                       :message => 'must be url for gif, jpg or png image' 
+  validates_length_of :title, :minimum => 10
   
   protected
     def price_must_be_at_least_one_cent
