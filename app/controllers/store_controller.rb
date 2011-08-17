febc5580a83e68c11ff4214eeeef11baa51f0853
laelingur <laelingur@gmail.com>
2011-08-16 23:36:44 -0500

    added shopping cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,8 +3,33 @@ class StoreController < ApplicationController
     @products = Product.find_products_for_sale
   end
   
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart   = find_cart
+    @cart.add_product(product)
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Invalid Product")
+  end
+  
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
+  end
+  
+  def cart
+    @cart = find_cart
+  end
+  
   private 
   def find_cart
     session[:cart] ||= Cart.new
   end
+  
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action => 'index'
+  end
+  
+  
 end
